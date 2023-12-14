a08c84cab93d50349b6ed0bcb221c9c1fe4b9969
MirandaPCamelo <152246653+MirandaPCamelo@users.noreply.github.com>
2023-12-14 08:20:18 -0500

    Update Nodo.java

--- Nodo.java
+++ Nodo.java
@@ -1,10 +1,12 @@
-
 /**
- * Miranda Patricia Pérez Camelo
- * 10/12/2023
+ * Creación del nodo y los enlaces.
+ * 
+ * @author (Miranda Patricia Pérez Camelo) 
+ * @version (Noviembre 27, 2023)
  */
-public class Nodo{
-	public int dato;
-	public Nodo izquierdo;
-	public Nodo derecho;
+public class Node
+{
+        //String name;
+        Object name; //la clave del nodo (lo que tiene adentro)
+        Node next; //flechita de enlace para el siguiente nodo
 }
